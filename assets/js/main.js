c242a8b08c61ebc204250272b1398c6178e147b6
paukuman <sebatasangan2018@gmail.com>
2023-06-18 00:09:48 +0700

    asdasd

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,13 @@ async function fetchJson(url) {
     const json = await get.json();
     return json;
 }
+
+window.addEventListener('load', () => {
+    var prevHash = window.location.hash;
+    window.setInterval(function () {
+        if (window.location.hash != prevHash) {
+            prevHash = window.location.hash;
+            window.location.reload();
+        }
+    }, 100);
+});
