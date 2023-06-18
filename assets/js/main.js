2044e4522bb83042586fa47b62e70d8c14265fc7
paukuman <sebatasangan2018@gmail.com>
2023-06-18 10:36:31 +0700

    Changed #10

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,39 @@ async function fetchJson(url) {
     return json;
 }
 
+async function mainFunction() {
+    let data, json, open;
+    if (!window.location.hash) {
+        let { blocks } = await fetchJson('https://api.modules.my.id/v2/models');
+        
+        open = blocks.length;
+        json = blocks;
+        data = { json, part: 'assets/part/home.html' };
+    } else {
+        const username = window.location.hash.substring(1);
+        let cam = await fetchJson(`https://api.modules.my.id/v2/models/username/${username}/cam`);
+        
+        open = true;
+        json = cam.user;
+        data = { json, part: 'assets/part/home.html' };
+    }
+
+    return { data, open };
+}
+
+function showTitle(id) {
+    const json = {
+        countryGenderModels: "Featured Live Sex Shows",
+        mostPopularModels: "Popular Models",
+        couplesModels: "Couples Live Sex Cams",
+        newModel: "New & Trending Girls",
+        vrModels: "VR Cams",
+        topStreamsModels: "Top Free Live Webcams"
+    }
+
+    return json[id];
+}
+
 window.addEventListener('load', () => {
     var prevHash = window.location.hash;
     window.setInterval(function () {
